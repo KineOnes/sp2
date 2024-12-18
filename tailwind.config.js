2db8bd83d2e3ab3ac8a9c844b0b694cbff29547b
Kine Ones <kineones@Kines-MacBook-Air.local>
2024-12-18 15:12:25 +0100

    Fix errors regarding php config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"], // Add the path to your HTML files
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
