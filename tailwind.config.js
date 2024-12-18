aa213878777a13d36bd23e704647ba32b6ffe57a
Kine Ones <kineones@Kines-MacBook-Air.local>
2024-12-18 15:16:40 +0100

    Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html"], // Add the path to your HTML files
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: "#F6F0E5",          // Background for listings
+        olive: "#A8B885",          // Search bar background
+        lightBrown: "#C4B29B",     // Button background
+        darkText: "#333333",       // Text color for headings
+      },
+    },
   },
   plugins: [],
 }
